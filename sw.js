90343608ab522baf0f16c04efe04bd4a8afc866c
Hagai Reuveni <hagair@Hagais-MBP.home>
2019-04-15 10:36:58 +0300

    adding tect radar day site

--- sw.js
+++ sw.js
@@ -8,7 +8,11 @@ self.addEventListener('install', function(e) {
 				'sw.js',
 				'images/favicon.png',
 				'images/logo512.png',
-				'images/logo192.png'
+				'images/logo192.png',
+				'fonts/glyphicons-halflings-regular.eot',
+				'fonts/glyphicons-halflings-regular.svg',
+				'fonts/glyphicons-halflings-regular.ttf',
+				'fonts/glyphicons-halflings-regular.woff',
 			]);
 		})
 	);
